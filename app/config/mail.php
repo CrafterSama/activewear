de67ffe73c8a143e4c23db216fae0a7b5988d5b8
Julmer Olivero Ocando <jolivero.03@gmail.com>
2015-03-10 20:24:43 -0430

    Cambios en el Correo Electronico

--- app/config/mail.php
+++ app/config/mail.php
@@ -1,6 +1,19 @@
 <?php
 
+
 return array(
+'driver' => 'smtp',
+'host' => 'smtp.gmail.com',
+'port' => 587,
+'from' => array('address' => Configuration::getServiceEmail(), 'name' => Configuration::getCompanyName()),
+'encryption' => 'tls',
+'username' => 'cariocamailservice@gmail.com',
+'password' => 'carioca123456',
+'sendmail' => '/usr/sbin/sendmail -bs',
+'pretend' => false,
+);
+
+/*return array(
 
 	'driver' => 'sendmail',
 	'host' => 'smtp.gmail.com',
@@ -11,4 +24,4 @@ return array(
 	'password' => 'carioca123456',
 	'sendmail' => '/usr/sbin/sendmail -bs',
 	'pretend' => false,
-);
+);*/

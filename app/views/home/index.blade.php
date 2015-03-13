06d62696b0c5c61bac59cf62ed3c999642b8b0dd
Julmer Olivero Ocando <jolivero.03@gmail.com>
2015-03-12 23:20:47 -0430

    Cambios en el Index

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -21,16 +21,16 @@
      </div>
 </div>
 <div class="buttons col-md-8 col-xs-12">
-    <div class="img-1 col-xs-6 col-md-6">
-        <div class="row-fluid">
+    <div class="row-fluid">
+        <div class="img-1 col-xs-6 col-md-6">
             <img class="products col-md-12" src="/../assets/images/home_productos.png" alt="productos" width="100%">
             <div class="title-box carioca_color1">
                 <h3 class="fig-text"><a href="/productos/carioca" alt="Productos Carioca"><strong>Productos Carioca + </strong></a></h3>
             </div>
         </div>
     </div>
-    <div class="img-2 col-xs-6 col-md-6">
-        <div class="row-fluid">
+    <div class="row-fluid">
+        <div class="img-2 col-xs-6 col-md-6">
             <a href="/carrito"><img class="carrito col-md-12" src="/../assets/images/home_carrito.png" alt="carrito" width="100%"></a>
             <div class="title-box carioca_color4">
                 <h3 class="fig-text"><a href="/carrito" alt="Carrito de Compras"><strong>Carrito + </strong></a></h3>
@@ -39,7 +39,7 @@
     </div>
 </div>
 <div class="col-md-4">
-    <div class="col-xs-12 social-buttons">
+    <div class="col-md-12 social-buttons">
         <div class="row social">
             <div class="col-xs-4 carioca_color1">
                 <div class="social-box "><a class="btn btn-block btn-social btn-instagram" href="http://www.instagram.com/carioca_activewear"><i class="fa fa-instagram fa-lg fa-3x"></i></a></div>

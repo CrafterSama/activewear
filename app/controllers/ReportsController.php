8ff2986707718a87acc5a433b180c2bea8eace0e
Julmer Olivero Ocando <jolivero.03@gmail.com>
2015-05-08 18:21:46 -0430

    Changes in Reports

--- app/controllers/ReportsController.php
+++ app/controllers/ReportsController.php
@@ -4,14 +4,14 @@ class ReportsController extends \BaseController {
 
 	public function getDates()
 	{
-		if(!$this->autorizado) return Redirect::to('/login');
-		
+		//if(!$this->autorizado) return Redirect::to('/login');
+
 		return View::make('admin.reports');
 	}
 
 	public function postDates()
 	{
-		if(!$this->autorizado) return Redirect::to('/login');
+		//if(!$this->autorizado) return Redirect::to('/login');
 		
 		$dateStart 	= Input::get('date-start');
 		$dateEnd 	= Input::get('date-end');

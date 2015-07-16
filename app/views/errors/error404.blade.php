e263dceb6c20a6f998c7b272e1a53ac9485327a9
CrafterSama <jolivero.03@gmail.com>
2015-07-15 20:53:59 -0430

    Changes in Error Page

--- app/views/errors/error404.blade.php
+++ app/views/errors/error404.blade.php
@@ -1,24 +1,18 @@
 @extends('layouts.home')
 
 @section('content')
-	<div class="main">
-	<div class="container">
-    <div class="body-error">
-
+<div class="body-error">
     <div class="container ">
-      <br />
-      <section class="error-wrapper text-center">
-          <div class="error-head"> </div>
-          <h1><img src="assets/images/404.png" alt=""></h1>
-          <div class="error-desk">
-              <h2>Modulo no Encontrado</h2>
-              <p class="nrml-txt">No podemos encontrar esta Pagina</p>
-          </div>
-          <a href="/" class="btn btn-success btn-lg"><i class="fa fa-home"></i> Regresar al Inicio</a>
-      </section>
-
-    </div>
-    </div>
-	</div>
-	</div>
+    <br />
+    <section class="error-wrapper text-center">
+      <div class="error-head"> </div>
+      <h1><img src="assets/images/404.png" alt=""></h1>
+      <div class="error-desk">
+          <h2>Modulo no Encontrado</h2>
+          <p class="nrml-txt">No podemos encontrar esta Pagina</p>
+      </div>
+      <a href="/" class="btn btn-success btn-lg"><i class="fa fa-home"></i> Regresar al Inicio</a>
+    </section>
+  </div>
+</div>
 @stop
